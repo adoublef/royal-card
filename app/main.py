3ccb6ec85fa762ee7fdcaa6e7683cd435c0abf7a
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-11-07 22:39:34 +0000

    add hello world endpoint

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from uvicorn import run
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def handle_index() -> str:
+    return "Hello, World!"    
+
+if __name__ == "__main__":
+    run("main:app", host="0.0.0.0", port=80, proxy_headers=True)
